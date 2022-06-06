3a34b2eba601da9e2752e743286678f50ea86c65
GiuliaV <valente.giulia93@gmail.com>
2022-06-06 15:59:29 +0200

    Script funziona

--- script/script.js
+++ script/script.js
@@ -3,3 +3,26 @@
 // generare 10 indirizzi email e stamparli in pagina all’interno di una lista.
 // Bonus:
 // Far comparire gli indirizzi email solamente quando sono stati tutti generati.
+
+var app = new Vue (
+    {
+        el:'#app',
+        
+        data: {
+            emailList: [],
+        },
+        methods: {
+        },
+        
+        mounted() {
+
+            for(let i = 0; i < 10; i++) {
+                axios.get('https://flynn.boolean.careers/exercises/api/random/mail').then((result)=> {
+                    this.email = result.data.response;
+                    this.emailList.push(this.email);
+                });
+            }
+            
+        },
+    }
+);
